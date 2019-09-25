fc993020d916be9b9741602b3032fb256da7d564
phamconganh <conganhphamnd@gmail.com>
2019-09-25 17:46:51 +0700

    update button bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:uet_comic/src/pages/account.dart';
+import 'package:uet_comic/src/pages/home.dart';
+import 'package:uet_comic/src/pages/search.dart';
+import 'package:uet_comic/src/pages/settings.dart';
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -18,92 +23,123 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _selectedIndex = 0;
 
-  void _incrementCounter() {
+  // bool _showAppbar = true; //this is to show app bar
+  // ScrollController _scrollBottomBarController = ScrollController(); // set controller on scrolling
+  // bool isScrollingDown = false;
+  // bool _show = true;
+
+  // void showBottomBar() {
+  //   setState(() {
+  //     _show = true;
+  //   });
+  // }
+
+  // void hideBottomBar() {
+  //   setState(() {
+  //     _show = false;
+  //   });
+  // }
+
+  // void _onTap(int index) {
+  //   pageController.jumpToPage(index);
+  //   // _scaffoldKey.currentState.openDrawer();
+  // }
+
+  void onPageChanged(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
+  final pageController = PageController();
+  final bodyList = <Widget>[
+    HomePage(),
+    SearchPage(),
+    AccountPage(),
+    SettingsPage(),
+  ];
+
+  final items = <BottomNavyBarItem>[
+    BottomNavyBarItem(
+      icon: Icon(Icons.apps),
+      title: Text('Home'),
+      activeColor: Colors.red,
+    ),
+    BottomNavyBarItem(
+        icon: Icon(Icons.people),
+        title: Text('Users'),
+        activeColor: Colors.purpleAccent),
+    BottomNavyBarItem(
+        icon: Icon(Icons.message),
+        title: Text('Messages'),
+        activeColor: Colors.pink),
+    BottomNavyBarItem(
+        icon: Icon(Icons.settings),
+        title: Text('Settings'),
+        activeColor: Colors.blue),
+  ];
+
+  // void scrollToHide() async {
+  //   _scrollBottomBarController.addListener(() {
+  //     if (_scrollBottomBarController.position.userScrollDirection == ScrollDirection.reverse && !isScrollingDown) {
+  //       isScrollingDown = true;
+  //       _showAppbar = false;
+  //       hideBottomBar();
+  //     }
+  //     if (_scrollBottomBarController.position.userScrollDirection == ScrollDirection.forward && isScrollingDown) {
+  //       isScrollingDown = false;
+  //       _showAppbar = true;
+  //       showBottomBar();
+  //     }
+  //   });
+  // }
+
+  @override
+  void initState() {
+    super.initState();
+    // scrollToHide();
+  }
+
+  // @override
+  // void dispose() {
+  //   _scrollBottomBarController.removeListener(() {});
+  //   super.dispose();
+  // }
+
+  // final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>(); // ADD THIS LINE
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        body: PageView(
+          controller: pageController,
+          onPageChanged: onPageChanged,
+          children: bodyList,
+          physics: ClampingScrollPhysics(),
+          // physics: NeverScrollableScrollPhysics (), // No sliding
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        bottomNavigationBar: BottomNavyBar(
+            selectedIndex: _selectedIndex,
+            showElevation: true, // use this to remove appBar's elevation
+            onItemSelected: (index) => setState(() {
+                  _selectedIndex = index;
+                  pageController.animateToPage(index,
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.ease);
+                }),
+            items: items));
   }
 }
